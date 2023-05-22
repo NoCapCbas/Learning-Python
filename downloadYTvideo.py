873e8491cf4480e87d8a0bfa5b1132273f933235
NoCapCbas <56492050+NoCapCbas@users.noreply.github.com>
2023-05-22 07:42:27 -0400

    Update downloadYTvideo.py

--- downloadYTvideo.py
+++ downloadYTvideo.py
@@ -1,6 +1,9 @@
 import pytube
 link = 'https://www.youtube.com/watch?v=0sMtoedWaf0'
-video_download = pytube.Youtube(link)
-video_download.streams.first().download()
+# poor quality video
+# video_download = pytube.YouTube(link)
+# video_download.streams.first().download()
+
+# high quality video
 video_download = pytube.Youtube(link)
 video_download.streams.filter(progressive=True).last().download()
